2ac2942e907f06f97475a5a8f384a1e04c902498
Jesus Paz <jeherpabo@gmail.com>
2018-05-10 18:46:52 -0500

    Agrega metodos a la controladora, commit y conectar

--- FinalBD/src/mundo/Controladora.java
+++ FinalBD/src/mundo/Controladora.java
@@ -1,7 +1,47 @@
 package mundo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Controladora {
 
+	private static Connection con;
+	
+	
+	public static Boolean ConectarYConsultar(String usuario, String contraseña ) {
+		try {
+			
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			 con = DriverManager.getConnection(
+					"jdbc:oracle:thin:@200.3.193.24:1522:ESTUD",
+					usuario,
+					contraseña);
+			 
+			 
+			 return true;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static void commit() {
+		
+		try {
+			Statement stmt =  con.createStatement();
+			Boolean re = stmt.execute(
+					"COMMIT");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+	}
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
