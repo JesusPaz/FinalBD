90813345628668210f371b9a2cd8e86dd4777e6c
AlejoAlvarado <36465658+AlejoAlvarado@users.noreply.github.com>
2018-05-16 09:55:31 -0500

    Update Clientes.sql

--- Paquetes/Clientes.sql
+++ Paquetes/Clientes.sql
@@ -1,23 +1,34 @@
 
 
+
+
 CREATE OR REPLACE PACKAGE pkClientes AS -- spec
-PROCEDURE pInsertarCliente
-(ivNombre VARCHAR2, ivCedula VARCHAR2, ivFechaNacimiento VARCHAR2, ivDirección VARCHAR2 ,ivTeléfono VARCHAR2);
-PROCEDURE pEliminarCliente(iCedulaCLiente VARCHAR2);
+    PROCEDURE pInsertarCliente
+        (ivNombre VARCHAR2, ivCedula VARCHAR2, ivFechaNacimiento DATE, ivDireccion VARCHAR2 ,ivTelefono VARCHAR2);
+    PROCEDURE pEliminarCliente(iCedulaCLiente VARCHAR2);
+    PROCEDURE pActualizarCliente
+        (ivCedulaBuscada VARCHAR2,ivNombre VARCHAR2, ivCedulaNueva VARCHAR2, ivFechaNacimiento DATE, ivDireccion VARCHAR2 ,ivTelefono VARCHAR2);
 END pkClientes;
 /
 CREATE OR REPLACE PACKAGE BODY pkClientes AS -- body
--- Implementación Procedimiento 1
-PROCEDURE pInsertarCliente
-(ivNombre VARCHAR2, ivCedula VARCHAR2, ivFechaNacimiento VARCHAR2, ivDirección VARCHAR2 ,ivTeléfono VARCHAR2)
-IS
-BEGIN
-INSERT INTO cliente
-VALUES (ivNombre, ivCedula, ivFechaNacimiento, ivDirección, ivTeléfono);
-END pInsertarCliente;
--- Implementación Procedimiento 2
-PROCEDURE pEliminarCliente(iCedulaCliente IN NUMBER) IS
-BEGIN
-DELETE FROM cliente WHERE cedula = iCedulaCliente;
-END pEliminarCliente;
+-- Insertar
+  PROCEDURE pInsertarCliente
+    (ivNombre VARCHAR2, ivCedula VARCHAR2, ivFechaNacimiento DATE, ivDireccion VARCHAR2 ,ivTeléfono VARCHAR2)
+  IS
+    BEGIN
+    INSERT INTO cliente
+    VALUES (ivNombre, ivCedula, ivFechaNacimiento, ivDirección, ivTeléfono);
+  END pInsertarCliente;
+-- Eliminar
+  PROCEDURE pEliminarCliente(iCedulaCliente IN NUMBER) IS
+    BEGIN
+    DELETE FROM cliente WHERE cedula = iCedulaCliente;
+  END pEliminarCliente;
+-- Actualizar
+  PROCEDURE pActualizarCliente(ivCedulaBuscada VARCHAR2,ivNombre VARCHAR2, ivCedulaNueva VARCHAR2, ivFechaNacimiento DATE, ivDireccion VARCHAR2 ,ivTelefono VARCHAR2) IS
+    BEGIN
+    UPDATE cliente 
+    SET nombre=ivNombre, cedula=ivCedula, fechaNacimiento=ivFechaNacimiento, direccion=ivDireccion, telefono=ivTelefono
+    WHERE cedula=ivCedulaBuscada;
+  END pActualizarCliente;
 END pkClientes;
