9b09ce8101bb8c7b1cebb2faafcd851dfb1807cd
Diana Isabel Torres Rojas <diana.torres6@correo.icesi.edu.co>
2018-05-21 14:30:11 -0500

    paquete con Funcionario, si funciona.

---
+++ Paquetes/Nivel1/Funcionario.sql
@@ -0,0 +1,67 @@
+CREATE OR REPLACE PACKAGE pkFuncionarioNivel1 IS 
+
+    PROCEDURE pInsertarFuncionario(cedulaFuncionario IN VARCHAR2, nombre IN VARCHAR2, fechaNacimiento IN DATE, direccion IN VARCHAR2,telefono IN VARCHAR2  );
+   
+    PROCEDURE pEliminarFuncionario(cedulaFuncionario IN VARCHAR2);
+    
+    PROCEDURE pActualizarFuncionario(cedulaFuncionario IN VARCHAR2, inombre IN VARCHAR2, fechaNacimiento IN DATE, direccion IN VARCHAR2,telefono IN VARCHAR2 );
+   
+    FUNCTION fObtenerFuncionario(cedulaFuncionario IN VARCHAR2) RETURN FUNCIONARIO%rowtype;
+    
+END pkFuncionarioNivel1;
+/
+CREATE OR REPLACE PACKAGE BODY pkFuncionarioNivel1 IS 
+-- Insertar
+  PROCEDURE pInsertarFuncionario
+    (cedulaFuncionario IN VARCHAR2, nombre IN VARCHAR2, fechaNacimiento IN DATE, direccion IN VARCHAR2,telefono IN VARCHAR2)
+  IS
+    BEGIN
+    INSERT INTO FUNCIONARIO
+    VALUES (cedulaFuncionario, nombre , fechaNacimiento, direccion,telefono );
+    EXCEPTION
+        WHEN DUP_VAL_ON_INDEX THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error, este registro ya existe.');
+        WHEN OTHERS THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error desconocido.'||SQLERRM||SQLCODE);
+  END pInsertarFuncionario;
+-- Eliminar
+  PROCEDURE pEliminarFuncionario(cedulaFuncionario IN VARCHAR2) IS
+    BEGIN
+    DELETE FROM FUNCIONARIO T WHERE T.CEDULAFUNCIONARIO= cedulaFuncionario;
+     EXCEPTION
+        WHEN NO_DATA_FOUND THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error, no existe un funcionario con esa cédula.');
+        WHEN OTHERS THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error desconocido.'||SQLERRM||SQLCODE);
+  END pEliminarFuncionario;
+-- Actualizar
+  PROCEDURE pActualizarFuncionario(cedulaFuncionario IN VARCHAR2, inombre IN VARCHAR2, fechaNacimiento IN DATE, direccion IN VARCHAR2,telefono IN VARCHAR2)
+  IS
+    BEGIN
+    UPDATE FUNCIONARIO 
+    SET  NOMBRE=inombre 
+    WHERE CEDULAFUNCIONARIO=cedulaFuncionario;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error,no existe un funcionario con esa cédula.');
+        WHEN OTHERS THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error desconocido.'||SQLERRM||SQLCODE);
+  END pActualizarFuncionario;
+  
+  FUNCTION fObtenerFuncionario(cedulaFuncionario IN VARCHAR2) RETURN FUNCIONARIO%rowtype 
+    IS ovFuncionario FUNCIONARIO%rowtype;
+    BEGIN
+        SELECT * into ovFuncionario
+        FROM FUNCIONARIO
+        WHERE CEDULAFUNCIONARIO=cedulaFuncionario;
+        EXCEPTION
+        WHEN NO_DATA_FOUND THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error,no existe un funcionario con esa cédula.');
+        WHEN OTHERS THEN 
+        RAISE_APPLICATION_ERROR(-20001,'Error desconocido.'||SQLERRM||SQLCODE);
+    END fObtenerFuncionario;
+
+
+END pkFuncionarioNivel1;
+
+
