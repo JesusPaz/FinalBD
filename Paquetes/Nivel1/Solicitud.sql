6f5131cc38f44955279e674e8821abf57fce907d
Jesus Paz <jeherpabo@gmail.com>
2018-05-21 11:59:52 -0500

    Agrega Solicitud a nivel 1

---
+++ Paquetes/Nivel1/Solicitud.sql
@@ -0,0 +1,49 @@
+
+CREATE OR REPLACE PACKAGE pkSolicitudNivel1 IS 
+
+    PROCEDURE pInsertarSolicitud(ivIdSolicitud IN NUMBER, ivEstado IN VARCHAR2,ivObservacion IN VARCHAR2,ivIdCliente IN VARCHAR2,ivIdFuncionario IN VARCHAR2
+    ,ivFechaIni IN DATE,ivFechaFin IN DATE,ivTipoSolicitud IN NUMBER,ivTipoAnomalia IN NUMBER,ivIdTipoProducto IN NUMBER,ivIdProducto IN NUMBER );
+   
+    PROCEDURE pEliminarSolicitud(ivIdSolicitud IN NUMBER);
+    
+    PROCEDURE pActualizarSolicitud(ivIdSolicitud IN NUMBER, ivEstado IN VARCHAR2,ivObservacion IN VARCHAR2,ivIdCliente IN VARCHAR2,ivIdFuncionario IN VARCHAR2
+    ,ivFechaIni IN DATE,ivFechaFin IN DATE,ivTipoSolicitud IN NUMBER,ivTipoAnomalia IN NUMBER,ivIdTipoProducto IN NUMBER,ivIdProducto IN NUMBER );
+   
+    FUNCTION fObtenerSolicitud(ivIdSolicitud in NUMBER) RETURN SOLICITUD%rowtype;
+    
+END pkSolicitudNivel1;
+/
+CREATE OR REPLACE PACKAGE BODY pkSolicitudNivel1 IS 
+-- Insertar
+  PROCEDURE pInsertarSolicitud
+    (ivIdSolicitud NUMBER, ivEstado VARCHAR2,ivObservacion VARCHAR2,ivIdCliente VARCHAR2,ivIdFuncionario VARCHAR2
+    ,ivFechaIni DATE,ivFechaFin DATE,ivTipoSolicitud NUMBER,ivTipoAnomalia NUMBER,ivIdTipoProducto NUMBER,ivIdProducto NUMBER )
+  IS
+    BEGIN
+    INSERT INTO SOLICITUD
+    VALUES (ivIdSolicitud , ivEstado ,ivObservacion ,ivFechaIni,ivFechaFin,ivIdCliente 
+    ,ivTipoSolicitud ,ivIdFuncionario,ivTipoAnomalia,ivIdTipoProducto ,ivIdProducto );
+  END pInsertarSolicitud;
+-- Eliminar
+  PROCEDURE pEliminarSolicitud(ivIdSolicitud IN NUMBER) IS
+    BEGIN
+    DELETE FROM SOLICITUD S WHERE S.IDSOLICITUD = ivIdSolicitud;
+  END pEliminarSolicitud;
+-- Actualizar
+  PROCEDURE pActualizarSolicitud(ivIdSolicitud NUMBER, ivEstado VARCHAR2,ivObservacion VARCHAR2,ivIdCliente VARCHAR2,ivIdFuncionario VARCHAR2
+    ,ivFechaIni DATE,ivFechaFin DATE,ivTipoSolicitud NUMBER,ivTipoAnomalia NUMBER,ivIdTipoProducto NUMBER,ivIdProducto NUMBER ) IS
+    BEGIN
+    UPDATE SOLICITUD 
+    SET  Estado=ivEstado ,Observacion=ivObservacion ,FechaIni=ivFechaIni,FechaFin=ivFechaFin,CLIENTE_CEDULACLIENTE=ivIdCliente 
+    ,TIPOSOLICITUD_IDTIPOSOLICITUD=ivTipoSolicitud ,FUNCIONARIO_CEDULAFUNCIONARIO=ivIdFuncionario,TIPOANOMALIA_IDANOMALIA=ivTipoAnomalia
+    ,TIPOPRODUCTO_IDTIPOPRODUCTO=ivIdTipoProducto ,PRODUCTO_IDPRODUCTO=ivIdProducto 
+    WHERE IDSOLICITUD=ivIdSolicitud;
+  END pActualizarSolicitud;
+  FUNCTION fObtenerSolicitud(ivIdSolicitud IN NUMBER) RETURN SOLICITUD%rowtype 
+    IS ovSolicitud SOLICITUD%rowtype;
+    BEGIN
+        SELECT * into ovSolicitud
+        FROM Solicitud
+        WHERE IDSOLICITUD=ivIdSolicitud;
+    END fObtenerSolicitud;
+END pkSolicitudNivel1;
