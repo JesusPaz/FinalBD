f38049fda4cf85ff5c63d7030fe7b301a30c1857
Jesus Paz <jeherpabo@gmail.com>
2018-05-21 21:48:03 -0500

    paquete nivel 3

---
+++ Paquetes/Nivel3/pkRegistroNivel3.sql
@@ -0,0 +1,27 @@
+
+CREATE OR REPLACE PACKAGE pkRegistroNivel3 IS 
+
+    PROCEDURE pRegistarSolicitud(ivIdSolicitud IN NUMBER,ivObservacion IN VARCHAR2,ivIdCliente IN VARCHAR2,ivTipoSolicitud IN NUMBER
+    ,ivTipoAnomalia NUMBER,ivIdTipoProducto IN NUMBER,ivIdProducto IN NUMBER, ovRetorno out VARCHAR2);
+    
+END pkRegistroNivel3;
+/
+CREATE OR REPLACE PACKAGE BODY pkRegistroNivel3 IS 
+-- Insertar
+  PROCEDURE pRegistarSolicitud(ivIdSolicitud IN NUMBER,ivObservacion IN VARCHAR2,ivIdCliente IN VARCHAR2,ivTipoSolicitud IN NUMBER
+    ,ivTipoAnomalia NUMBER,ivIdTipoProducto IN NUMBER,ivIdProducto IN NUMBER, ovRetorno out VARCHAR2)
+    IS
+    BEGIN
+    
+    PKSOLICITUDNIVEL1.pInsertar(ivIdSolicitud ,'Pendiente',ivObservacion,ivIdCliente ,'000' 
+    ,ivTipoSolicitud,ivTipoAnomalia,ivIdTipoProducto ,ivIdProducto );
+    
+    PKREGISTRONIVEL2.pRegistarSolicitud(ivIdSolicitud,ivObservacion,ivIdCliente
+    ,ivTipoSolicitud,ivTipoAnomalia,ivIdTipoProducto ,ivIdProducto);
+    ovRetorno:='Solicitud registrada correctamente.';
+    EXCEPTION
+        WHEN OTHERS THEN 
+        ovRetorno:=SQLERRM;
+  END pRegistarSolicitud;
+    
+END pkRegistroNivel3;
