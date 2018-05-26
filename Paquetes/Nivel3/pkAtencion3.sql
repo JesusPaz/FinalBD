b7b80e6036343478715327ee7ade95f6d991b713
AlejoAlvarado <36465658+AlejoAlvarado@users.noreply.github.com>
2018-05-25 23:12:02 -0500

    Add files via upload

---
+++ Paquetes/Nivel3/pkAtencion3.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE PACKAGE pkAtencionNivel3 IS 
+    PROCEDURE pAtenderSolicitud(ivIdCedulaFuncionario VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2);
+    PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2);
+    PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2);
+   
+END pkAtencionNivel3;
+/
+CREATE OR REPLACE PACKAGE BODY pkAtencionNivel3 IS 
+
+   PROCEDURE pAtenderSolicitud(ivIdCedulaFuncionario VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2)
+   IS 
+   BEGIN 
+   
+   PKASIGNACIONNIVEL2.pAtenderSolicitud(ivIdCedulaFuncionario,ivIdSolicitud, ivComentario);
+   ovRetorno:='Se atendio la solicitud de manera exitosa.';
+     EXCEPTION
+        
+        WHEN OTHERS THEN 
+        ovRetorno:=SQLERRM; 
+   
+   END pAtenderSolicitud;
+    
+   PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2)
+   IS
+   BEGIN
+   
+   
+   PKASIGNACIONNIVEL2.pAtenderReclamoODano(ivIdCedulaFuncionario ,ivIdSolicitud, ivEstado);
+   ovRetorno:='Se atendio el reclamo o el daño de manera exitosa.';
+     EXCEPTION
+        WHEN OTHERS THEN 
+        ovRetorno:=SQLERRM; 
+   END pAtenderReclamoODano;
+   
+   
+   PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2)
+   IS
+   BEGIN
+   
+   
+   PKASIGNACIONNIVEL2.pAtenderReclamoODanoAutomatico(ivIdSolicitud);
+   ovRetorno:='Se realizo la atencion automatica de manera exitosa.';
+     EXCEPTION
+        WHEN OTHERS THEN 
+        ovRetorno:=SQLERRM; 
+   END pAtenderReclamoODanoAutomatico;
+      
+
+
+END pkAtencionNivel3;
