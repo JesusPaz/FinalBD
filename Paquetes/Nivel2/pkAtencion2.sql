39f8598110ab09fac62d7ac98db3ddf9d4d34c34
AlejoAlvarado <36465658+AlejoAlvarado@users.noreply.github.com>
2018-05-26 00:10:09 -0500

    Update pkAtencion2.sql

--- Paquetes/Nivel2/pkAtencion2.sql
+++ Paquetes/Nivel2/pkAtencion2.sql
@@ -1,60 +1,66 @@
 CREATE OR REPLACE PACKAGE pkAtencionNivel2 IS 
-   PROCEDURE pAtenderSolicitud(ivIdCedulaFuncionario VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2);
-   PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2);
-   PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2);
+   PROCEDURE pAtenderSolicitud(ivCedulaFuncionario IN VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2);
+   PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2,ivComentario IN VARCHAR2);
 END pkAtencionNivel2;
 /
 CREATE OR REPLACE PACKAGE BODY pkAtencionNivel2 IS 
 
-   PROCEDURE pAtenderSolicitud(ivIdCedulaFuncionario IN VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2)--p1
+   PROCEDURE pAtenderSolicitud(ivCedulaFuncionario IN VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2)
    IS 
    fecha DATE;
-   solicitud SOLICITUD%rowtype;
+   solicitudAct SOLICITUD%rowtype;
+
+ 
    BEGIN 
-   solicitud :=pkSOLICITUD.OBTENERSOLICITUD(ivIdSolicitud);
-   
-   IF solicitud IS NULL THEN
+   solicitudAct :=pkSOLICITUDNIVEL1.fCONSULTAR(ivIdSolicitud);
+ 
+
+    
+   IF solicitudAct.IDSOLICITUD IS NULL THEN
    RAISE_APPLICATION_ERROR(-20001,'No existe una solicitud con el ID buscado.');
    END IF;
    
-   IF solicitud.FUNCIONARIO_CEDULAFUNCIONARIO=ivIdCedulaFuncionario THEN     
-    IF solicitud.TIPOSOLICITUD_IDTIPOSOLICITUD='001' THEN
-    fecha:=ADDMONTHS(TRUNC(SYSDATE,'MM'),1);--El primer dia del proximo mes
-    pkPRODUCTOSXCLIENTENIVEL1.PINSERTAR(solicitud.PRODUCTO_IDPRODUCTO,CLIENTE_CEDULACLIENTE,fecha,NULL);
-    pkSOLICITUDNIVEL1.PMODIFICAR(solicitud.IDSOLICITUD, 'atendida',ivComentario,solicitud.FECHAINI,SYSDATE,solicitud.CLIENTE_CEDULACLIENTE,
-    solicitud.TIPOSOLICITUD_IDTIPOSOLICITUD,solicitud.FUNCIONARIO_CEDULAFUNCIONARIO,solicitud.TIPOANOMALIA_IDANOMALIA,solicitud.TIPOPRODUCTO_IDTIPOPRODUCTO,
-    solicitud.PRODUCTO_IDPRODUCTO);
-    ELSIF solicitud.TIPOSOLICITUD_IDTIPOSOLICITUD='002' THEN
+   IF solicitudAct.FUNCIONARIO_CEDULAFUNCIONARIO=ivCedulaFuncionario THEN     
+    IF solicitudAct.TIPOSOLICITUD_IDTIPOSOLICITUD='001' THEN
+    fecha:=ADD_MONTHS(TRUNC(SYSDATE,'MM'),1);--El primer dia del proximo mes
+    pkPRODUCTOSXCLIENTENIVEL1.PINSERTAR(solicitudAct.PRODUCTO_IDPRODUCTO,solicitudAct.CLIENTE_CEDULACLIENTE,fecha,NULL);
+    pkSOLICITUDNIVEL1.PMODIFICAR(solicitudAct.IDSOLICITUD, 'atendida',ivComentario,solicitudAct.CLIENTE_CEDULACLIENTE,solicitudAct.FUNCIONARIO_CEDULAFUNCIONARIO,
+    solicitudAct.FECHAINI,SYSDATE, solicitudAct.TIPOSOLICITUD_IDTIPOSOLICITUD,solicitudAct.TIPOANOMALIA_IDANOMALIA,solicitudAct.TIPOPRODUCTO_IDTIPOPRODUCTO,
+    solicitudAct.PRODUCTO_IDPRODUCTO);
+    
+    ELSIF solicitudAct.TIPOSOLICITUD_IDTIPOSOLICITUD='002' THEN
     fecha:=SYSDATE;
-    pkPRODUCTOSXCLIENTENIVEL1.PINSERTAR(solicitud.PRODUCTO_IDPRODUCTO,CLIENTE_CEDULACLIENTE,fecha,NULL);  
-    pkSOLICITUDNIVEL1.PMODIFICAR(solicitud.IDSOLICITUD, 'atendida',ivComentario,solicitud.FECHAINI,solicitud.FECHAFIN,solicitud.CLIENTE_CEDULACLIENTE,
-    solicitud.TIPOSOLICITUD_IDTIPOSOLICITUD,solicitud.FUNCIONARIO_CEDULAFUNCIONARIO,solicitud.TIPOANOMALIA_IDANOMALIA,solicitud.TIPOPRODUCTO_IDTIPOPRODUCTO,
-    solicitud.PRODUCTO_IDPRODUCTO);
+    pkPRODUCTOSXCLIENTENIVEL1.PINSERTAR(solicitudAct.PRODUCTO_IDPRODUCTO,solicitudAct.CLIENTE_CEDULACLIENTE,fecha,NULL);  
+    pkSOLICITUDNIVEL1.PMODIFICAR(solicitudAct.IDSOLICITUD, 'atendida',ivComentario,solicitudAct.CLIENTE_CEDULACLIENTE,solicitudAct.FUNCIONARIO_CEDULAFUNCIONARIO,
+    solicitudAct.FECHAINI,solicitudAct.FECHAFIN,solicitudAct.TIPOSOLICITUD_IDTIPOSOLICITUD,solicitudAct.TIPOANOMALIA_IDANOMALIA,solicitudAct.TIPOPRODUCTO_IDTIPOPRODUCTO,
+    solicitudAct.PRODUCTO_IDPRODUCTO);
     
     END IF;
    ELSE
    RAISE_APPLICATION_ERROR(-20001,'Esta solicitud no esta asociada al funcionario buscado.');
    END IF; 
+   
    END pAtenderSolicitud;
-    
-   PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2)--p2
+      
+PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2,ivComentario IN VARCHAR2)--p2
    IS
-   solicitud SOLICITUD%rowtype;
+   solicitudAct SOLICITUD%rowtype;
    BEGIN 
-   solicitud :=pkSOLICITUD.OBTENERSOLICITUD(ivIdSolicitud);
+   solicitudAct :=pkSOLICITUDNIVEL1.fCONSULTAR(ivIdSolicitud);
   
-   IF solicitud IS NULL THEN
+   IF solicitudAct.IDSOLICITUD IS NULL THEN
    RAISE_APPLICATION_ERROR(-20001,'No existe una solicitud con el ID buscado.');
    END IF;
-   IF solicitud.FUNCIONARIO_CEDULAFUNCIONARIO=ivIdCedulaFuncionario THEN     
-    IF solicitud.TIPOSOLICITUD_IDTIPOSOLICITUD='001' THEN
-    pkSOLICITUDNIVEL1.PMODIFICAR(solicitud.IDSOLICITUD, ivEstado,ivComentario,solicitud.FECHAINI,solcitud.FECHAFIN,solicitud.CLIENTE_CEDULACLIENTE,
-    solicitud.TIPOSOLICITUD_IDTIPOSOLICITUD,solicitud.FUNCIONARIO_CEDULAFUNCIONARIO,solicitud.TIPOANOMALIA_IDANOMALIA,solicitud.TIPOPRODUCTO_IDTIPOPRODUCTO,
-    solicitud.PRODUCTO_IDPRODUCTO);
+   IF solicitudAct.FUNCIONARIO_CEDULAFUNCIONARIO=ivIdCedulaFuncionario THEN     
+    IF solicitudAct.TIPOSOLICITUD_IDTIPOSOLICITUD='001' THEN
+    pkSOLICITUDNIVEL1.PMODIFICAR(solicitudAct.IDSOLICITUD, ivEstado,ivComentario,solicitudAct.CLIENTE_CEDULACLIENTE,solicitudAct.FUNCIONARIO_CEDULAFUNCIONARIO,solicitudAct.FECHAINI,solicitudAct.FECHAFIN,
+    solicitudAct.TIPOSOLICITUD_IDTIPOSOLICITUD,solicitudAct.TIPOANOMALIA_IDANOMALIA,solicitudAct.TIPOPRODUCTO_IDTIPOPRODUCTO,
+    solicitudAct.PRODUCTO_IDPRODUCTO);
     END IF;
    END IF;
    END pAtenderReclamoODano;
-   
+
+
    PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2)--p3
     IS
    solicitud SOLICITUD%rowtype;
