1ee9194b1659eb82d3e64981e9fdfd119a5f9dcb
AlejoAlvarado <36465658+AlejoAlvarado@users.noreply.github.com>
2018-05-27 18:16:55 -0500

    Update pkAtencion3.sql

--- Paquetes/Nivel3/pkAtencion3.sql
+++ Paquetes/Nivel3/pkAtencion3.sql
@@ -1,17 +1,31 @@
 CREATE OR REPLACE PACKAGE pkAtencionNivel3 IS 
+    PROCEDURE pValidarAtencion(ivCedulaFuncionario IN VARCHAR2,ivIdSolicitud IN NUMBER, ovRetorno out VARCHAR2);
     PROCEDURE pAtenderSolicitud(ivIdCedulaFuncionario VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2, ovRetorno out VARCHAR2);
-    PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2,ivComentario IN VARCHAR2, ovRetorno our VARCHAR2);
-    PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2);
+    PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2,ivComentario IN VARCHAR2, ovRetorno out VARCHAR2);
+    PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2, ovRetorno out VARCHAR2);
    
 END pkAtencionNivel3;
 /
 CREATE OR REPLACE PACKAGE BODY pkAtencionNivel3 IS 
 
+   PROCEDURE pValidarAtencion(ivCedulaFuncionario VARCHAR2,ivIdSolicitud IN NUMBER, ovRetorno out VARCHAR2)
+   IS 
+   BEGIN 
+   
+   PKATENCIONNIVEL2.fValidarAtencion(ivCedulaFuncionario,ivIdSolicitud);
+   ovRetorno:='Se atendio la solicitud de manera exitosa.';
+     EXCEPTION
+        
+        WHEN OTHERS THEN 
+        ovRetorno:=SQLERRM; 
+   
+   END pValidarAtencion;
+
    PROCEDURE pAtenderSolicitud(ivIdCedulaFuncionario VARCHAR2,ivIdSolicitud IN NUMBER, ivComentario IN VARCHAR2, ovRetorno out VARCHAR2)
    IS 
    BEGIN 
    
-   PKASIGNACIONNIVEL2.pAtenderSolicitud(ivIdCedulaFuncionario,ivIdSolicitud, ivComentario);
+   PKATENCIONNIVEL2.pAtenderSolicitud(ivIdCedulaFuncionario,ivIdSolicitud, ivComentario);
    ovRetorno:='Se atendio la solicitud de manera exitosa.';
      EXCEPTION
         
@@ -20,12 +34,12 @@ CREATE OR REPLACE PACKAGE BODY pkAtencionNivel3 IS
    
    END pAtenderSolicitud;
     
-   PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2, ovRetorno out VARCHAR2)
+   PROCEDURE pAtenderReclamoODano(ivIdCedulaFuncionario IN NUMBER,ivIdSolicitud IN VARCHAR2, ivEstado IN VARCHAR2,ivComentario IN VARCHAR2, ovRetorno out VARCHAR2)
    IS
    BEGIN
    
    
-   PKASIGNACIONNIVEL2.pAtenderReclamoODano(ivIdCedulaFuncionario ,ivIdSolicitud, ivEstado,ivComentario);
+   PKATENCIONNIVEL2.pAtenderReclamoODano(ivIdCedulaFuncionario ,ivIdSolicitud, ivEstado,ivComentario);
    ovRetorno:='Se atendio el reclamo o el daño de manera exitosa.';
      EXCEPTION
         WHEN OTHERS THEN 
@@ -33,12 +47,12 @@ CREATE OR REPLACE PACKAGE BODY pkAtencionNivel3 IS
    END pAtenderReclamoODano;
    
    
-   PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2)
+   PROCEDURE pAtenderReclamoODanoAutomatico(ivIdSolicitud IN VARCHAR2, ovRetorno out VARCHAR2)
    IS
    BEGIN
    
    
-   PKASIGNACIONNIVEL2.pAtenderReclamoODanoAutomatico(ivIdSolicitud);
+   PKATENCIONNIVEL2.pAtenderReclamoODanoAutomatico(ivIdSolicitud);
    ovRetorno:='Se realizo la atencion automatica de manera exitosa.';
      EXCEPTION
         WHEN OTHERS THEN 
